2bf1960da41efe936fea7da45526e359633327bd
Utsav kateliya <165784143+utsavkateliya@users.noreply.github.com>
2024-11-18 18:31:18 +0530

    Update admin.py

--- client_project_app/admin.py
+++ client_project_app/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Client, Project
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display=('client_name','created_at','updated_at','created_by',)
+    list_display=('id','client_name','created_at','updated_at','created_by',)
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display=('project_name','client','created_at','created_by')
