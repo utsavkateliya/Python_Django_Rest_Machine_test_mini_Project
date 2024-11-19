e462e47ba028683fec72fc33251671e84b85551b
Utsav kateliya <165784143+utsavkateliya@users.noreply.github.com>
2024-11-19 18:09:21 +0530

    Update admin.py

--- client_project_app/admin.py
+++ client_project_app/admin.py
@@ -9,10 +9,6 @@ class ClientAdmin(admin.ModelAdmin):
 class ProjectAdmin(admin.ModelAdmin):
     list_display=('project_name','client','created_at','created_by')
 
-    # def client_name(self, obj):
-    #     return obj.client.client_name  # Retrieve client_name from the related Client model
-    # client_name.admin_order_field = 'client__client_name'  # Allow sorting by client_name
-    # client_name.short_description = 'Client'
 
 admin.site.register(Client, ClientAdmin)
 admin.site.register(Project, ProjectAdmin)
